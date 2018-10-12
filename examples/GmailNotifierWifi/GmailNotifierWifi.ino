5ccd8a49931132da8a7c5d5df38ec4124bdb6088
Samuel Trassare <samuel.trassare@gmail.com>
2018-10-12 05:18:41 +0200

    Refining gmail notifier sketch

--- examples/GmailNotifierWifi/GmailNotifierWifi.ino
+++ examples/GmailNotifierWifi/GmailNotifierWifi.ino
@@ -17,8 +17,6 @@
 #include <mip_esp8266.h>
 #include <WiFiClientSecure.h>   // Include the HTTPS library
 
-
-
 //const char* ssid = "..............";                // Enter the SSID for your wifi network.
 //const char* password = "..............";            // Enter your wifi password.
 
@@ -37,12 +35,7 @@ const char* fingerprint = "D3 90 FC 82 07 E6 0D C2 CE F9 9D 79 7F EC F6 E6 3E CB
 // The Base64 encoded version of your Gmail login credentials (see below)
 const char* credentials = "ZW1haWwuYWRkcmVzc0BnbWFpbC5jb206cGFzc3dvcmQ=";
 
-const byte led = 13;
-
 void setup() {
-  // Don't need.
-  pinMode(led, OUTPUT);
-
   connectResult = mip.begin(ssid, password, hostname);
 
   if (!connectResult) {
@@ -50,7 +43,7 @@ void setup() {
     return;
   }
 
-  Serial1.print("IP address: ");               // You could delete this chunk of code.  It's just here
+  Serial1.print(F("IP address: "));               // You could delete this chunk of code.  It's just here
   Serial1.println(WiFi.localIP());             // to show your IP address.
 }
 
@@ -60,35 +53,35 @@ void loop() {
 
   int unread = getUnread();
   if (unread == 0) {
-    Serial.println("\r\nYou've got no unread emails");
-    digitalWrite(led, LOW);
+    Serial1.println(F("\r\nYou've got no unread emails"));
+    mip.writeChestLED(0x00, 0xFF, 0x00);  // Set chest to solid green.
   } else if (unread > 0) {
-    Serial.printf("\r\nYou've got %d new messages\r\n", unread);
-    digitalWrite(led, HIGH);
+    Serial1.printf("\r\nYou've got %d new messages\r\n", unread);
+    mip.writeChestLED(0x00, 0xFF, 0x00, 990, 980)
   } else {
-    Serial.println("Could not get unread mails");
+    Serial1.println(F("Could not get unread mails"));
   }
-  Serial.println('\n');
+  Serial1.println(F('\n'));
   delay(5000);
 }
 
 int getUnread() {    // a function to get the number of unread emails in your Gmail inbox
   WiFiClientSecure client; // Use WiFiClientSecure class to create TLS (HTTPS) connection
-  Serial.printf("Connecting to %s:%d ... \r\n", host, httpsPort);
+  Serial1.printf("Connecting to %s:%d ... \r\n", host, httpsPort);
   if (!client.connect(host, httpsPort)) {   // Connect to the Gmail server, on port 443
-    Serial.println("Connection failed");    // If the connection fails, stop and return
+    Serial1.println(F("Connection failed"));    // If the connection fails, stop and return
     return -1;
   }
 
   if (client.verify(fingerprint, host)) {   // Check the SHA-1 fingerprint of the SSL certificate
-    Serial.println("Certificate matches");
+    Serial1.println(F("Certificate matches"));
   } else {                                  // if it doesn't match, it's not safe to continue
-    Serial.println("Certificate doesn't match");
+    Serial1.println(F("Certificate doesn't match"));
     return -1;
   }
 
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
+  Serial1.print(F("Requesting URL: "));
+  Serial1.println(url);
 
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
@@ -96,7 +89,7 @@ int getUnread() {    // a function to get the number of unread emails in your Gm
                "User-Agent: ESP8266\r\n" +
                "Connection: close\r\n\r\n"); // Send the HTTP request headers
 
-  Serial.println("Request sent");
+  Serial1.println(F("Request sent"));
 
   int unread = -1;
 
@@ -109,9 +102,8 @@ int getUnread() {    // a function to get the number of unread emails in your Gm
       break;                                            // stop reading
     }                                                   // if the tag is not <fullcount>, repeat and read the next tag
   }
-  Serial.println("Connection closed");
+  Serial1.println("Connection closed");
 
   return unread;                                        // Return the number of unread emails
 }
-
 
